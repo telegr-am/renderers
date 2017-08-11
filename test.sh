de1b2fb900742e35de47bc38f75cdf06716ea633
Joe Barnes <joe@joescii.com>
2017-08-11 22:11:28 +0000

    Clean up

--- test.sh
+++ test.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 export RENDERER_HOME=$PWD
-pushd $(mkdir -p ${RENDERER_HOME}/tmp)
-  pushd $(mktemp -p ${RENDERER_HOME}/tmp -d)
-    export TEMP_DIR=$PWD
-    mkdir ./input
-    mkdir ./output
-    pushd ./input
-      git clone https://github.com/mojombo/mojombo.github.io.git ./main
-      touch layout.json
-    popd
-
-    docker run --rm \
-      --mount source=${TEMP_DIR}/input,target=/input,type=bind \
-      --mount source=${TEMP_DIR}/output,target=/output,type=bind \
-      renderer-jekyll
+pushd $(mktemp -p ${RENDERER_HOME}/tmp -d)
+  export TEMP_DIR=$PWD
+  mkdir ./input
+  mkdir ./output
+  pushd ./input
+    git clone https://github.com/mojombo/mojombo.github.io.git ./main
+    touch layout.json
   popd
+
+  docker run --rm \
+    --mount source=${TEMP_DIR}/input,target=/input,type=bind \
+    --mount source=${TEMP_DIR}/output,target=/output,type=bind \
+    renderer-jekyll
+
+  echo "Point your browser to ${TEMP_DIR}/output/index.html"
 popd
 
 
