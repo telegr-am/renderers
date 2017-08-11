5939abf9df95358d115d14720e14257b6b8db9d3
Joe Barnes <joe@joescii.com>
2017-08-11 19:32:23 +0000

    Now it complains about missing plugins

--- test.sh
+++ test.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 export RENDERER_HOME=$PWD
+pushd $(mkdir ${RENDERER_HOME}/tmp)
+  pushd $(mktemp -p ${RENDERER_HOME}/tmp -d)
+    export TEMP_DIR=$PWD
+    mkdir ./input
+    mkdir ./output
+    pushd ./input
+      git clone https://github.com/github/training-kit.git ./main
+      touch layout.json
+    popd
 
-pushd $(mktemp -d)
-  export TEMP_DIR=$PWD
-  mkdir ./input
-  mkdir ./output
-  pushd ./input
-    git clone https://github.com/github/training-kit.git ./main
-    touch layout.json
+    docker run --rm \
+      --mount source=${TEMP_DIR}/input,target=/input,type=bind \
+      --mount source=${TEMP_DIR}/output,target=/output,type=bind \
+      renderer-jekyll
   popd
-
-  docker run --rm \
-    --mount source=input,target=/input \
-    --mount source=output,target=/output \
-    renderer-jekyll
 popd
 
 
